161b89c5487547cc2199a9ec361b90403007254c
Nick Sweeting <github@sweeting.me>
2024-10-05 04:53:40 -0700

    fix publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,7 +6,7 @@ rm -Rf dist 2>/dev/null
 nano pyproject.toml
 git add -p .
 git commit -m "bump version to $1"
-git tag -f "v$1"
+git tag -f -a "v$1" -m "v$1"
 git push origin
 git push origin tags -f
 uv build
